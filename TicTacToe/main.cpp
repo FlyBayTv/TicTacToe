c01f9f4f25a5195ad115bfbd9ec363b73286c0fa
FlyBayTv <75486322+FlyBayTv@users.noreply.github.com>
2023-02-12 17:27:53 +0100

    comments

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -21,7 +21,7 @@ void menu() {
 		welcome();
 		cout << "\n\n";
 		cout << "MENU\n\n";
-		cout << "1: Singelplayer\n";
+		cout << "1: Singleplayer\n";
 		cout << "2: Multiplayer\n";
 		cout << "3: Help\n";
 		cout << "4: Quit\n\n\n";
@@ -48,11 +48,9 @@ void menu() {
 void multiplayer()
 {
 	char input = ' ';
-	//bool true für erster spieler false dan 2er spieler
 	bool player1Turn = true;
 	while (true) {
 		Feld();
-		// wir geben checkwin den wert nach dem er suchen soll 
 		if (checkWin('X')) {
 			cout << "\nPlayer1 has won\n\n";
 			return;
@@ -69,7 +67,6 @@ void multiplayer()
 		if (input == 'q') {
 			return;
 		}
-		//nach input von spieler eins wird die boolische variable negiert und so kann ich in einer anderen funkton abwechselnd die spieler spielen lassen
 		player1Turn = !player1Turn;
 	}
 }
@@ -81,7 +78,6 @@ char inputalternate(bool x) {
 		cout << "\n\nInput: ";
 		char input = ' ';
 		cin >> input;
-		// - '0' so das man eine char variable zu einer int variable machen kann
 		int inputNumber = input - '0';
 		if (inputNumber > 9 or inputNumber < 1) {
 			cout << "Invalid Input \n";
@@ -105,20 +101,15 @@ char inputalternate(bool x) {
 void KI(int diff) {
 	if (checkFull()) {
 		cout << "Gleichstant\n";
-		//singelplayer();
 		return;
 	}
-	// ki bassiert auf zufahl
 	if (diff == 1) {
 
 		int N = rand() % 3;
-		// 66 % das es 1 ist
 		if (N != 1) {
 			for (int i = 1; i < 10; i++) {
 				if (F[i] == ' ') {
 					F[i] = 'O';
-					/*überprüfen ob man mit O gewinnen kann wenn nicht zuweise ich ihm nicht das feld lehr ist das sieht der nutzer
-					aber ncht ki überprüft nur ob man da gewinnen kan*/
 					if (checkWin('O')) {
 						return;
 					}
@@ -192,12 +183,10 @@ void KI(int diff) {
 		return;
 	}
 	else if (diff == 3) {
-		//hard | schaue ob ki gewinnen kann
 		for (int i = 1; i < 10; i++) {
 			if (F[i] == ' ') {
 				F[i] = 'O';
-				/*überprüfen ob man mit O gewinnen kann wenn nicht zuweise ich ihm nicht das feld lehr ist das sieht der nutzer
-				aber ncht ki überprüft nur ob man da gewinnen kann*/
+				
 				if (checkWin('O')) {
 					return;
 				}
@@ -210,7 +199,7 @@ void KI(int diff) {
 		for (int i = 1; i < 10; i++) {
 			if (F[i] == ' ') {
 				F[i] = 'X';
-				// überprüfen ob spieler gewinnen kann
+				// Ã¼berprÃ¼fen ob spieler gewinnen kann
 				if (checkWin('X')) {
 					F[i] = 'O';
 					return;
@@ -220,14 +209,12 @@ void KI(int diff) {
 				}
 			}
 		}
-		//und stoppen ihn | schauen ob feld in der mitte frei ist 
-		// erstelle eine random variable nicht grösser as 4 und wenn es anders als 1 ist also 2 3 oder 4  ist dan setzt ehr auf feld numer 5 also mitte so das ehr nicht immer auf die mitte setzt wenn feld frei ist
+		
 		int mitte = rand() % 4;
 		if (F[5] == ' ' and mitte != 1) {
 			F[5] = 'O';
 			return;
 		}
-		//wenn nichts mehr hilf dan random feld auswehlen
 		while (true) {
 			srand(time(NULL));
 			int i = rand() % 9 + 1;
